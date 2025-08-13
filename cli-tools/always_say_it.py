19861082b29556f05942d11231a93def72b0eb16
Richard A. Forth <richard.forth@gmail.com>
2025-08-13 19:25:01 +0000

    Invoking a function in a script or on import, try it in ipython: import always_say_it

---
+++ cli-tools/always_say_it.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+If the above gives errors, on ubuntu, install python-is-python3
+Alternatively update the above to say python3 instead of python
+'''
+
+def say_it():
+    greeting = 'Hello'
+    target = 'Richard'
+    message = f'{greeting} {target}'
+    print(message)
+
+say_it()
