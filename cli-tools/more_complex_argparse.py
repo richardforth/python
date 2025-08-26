b8548673ddf5887b3ae2085bfa2d1523839b35a5
Richard A. Forth <richard.forth@gmail.com>
2025-08-26 17:48:01 +0100

    Add more verbose commentary

--- cli-tools/more_complex_argparse.py
+++ cli-tools/more_complex_argparse.py
@@ -18,29 +18,40 @@ def greet(greeting, name):
 
 
 if __name__ == "__main__":
+    # Set Application Title
     parser = argparse.ArgumentParser(description='Maritime control')
 
+    # add a --twice option to perform the same action twice
+    # NOT IMPLEMENTED HERE THOUGH
     parser.add_argument('--twice', '-t',
                         help='Do it twice',
                         action='store_true')
-    
+
+    # set groundwork for sub commands
     subparsers = parser.add_subparsers(dest='func')
 
+    # add first subcommand 'ships'
     ship_parser = subparsers.add_parser('ships',
                                         help='Ship related commands')
-    
+
+    # ships list, or ships sail 
     ship_parser.add_argument('command',
                              choices=['list', 'sail'])
-    
+
+    # add second subcommand 'sailors'
     sailor_parser = subparsers.add_parser('sailors',
                                         help='Talk to a sailor')
+
+    # sailors Jim => Ahoy there Jim
     sailor_parser.add_argument('name',
                              help='Sailors name')
-    
+
+    # sailors Jim -- greeting Bonjour -> Bonjour Jim
     sailor_parser.add_argument('--greeting', '-g',
                              help='Greeting',
                              default='Ahoy there')
-    
+
+    # start here
     args = parser.parse_args()
     if args.func == 'sailors':
         greet(args.greeting, args.name)
